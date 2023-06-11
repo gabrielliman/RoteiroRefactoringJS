f91b7d0c291acbda78db6a57ac72e7e9c57f6781
gabrielliman <gabrielliman2002@gmail.com>
2023-06-11 18:37:22 -0300

    Commit 2 - Extração de Função

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ function gerarFaturaStr (fatura, pecas) {
     const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
-  
-    for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = 0;
-  
-      switch (peca.tipo) {
+
+    function calcularTotalApresentacao(apre, peca) {
+      let total =0;
+      switch (peca.tipo){
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -29,6 +27,12 @@ function gerarFaturaStr (fatura, pecas) {
       default:
           throw new Error(`Peça desconhecia: ${peca.tipo}`);
       }
+      return total
+    }
+  
+    for (let apre of fatura.apresentacoes) {
+      const peca = pecas[apre.id];
+      let total = calcularTotalApresentacao(apre, peca)
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
